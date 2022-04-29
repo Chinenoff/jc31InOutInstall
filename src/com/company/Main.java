9567d8ccd036be90852097144d0754a3fee99c5d
ChinenOFF <pro100cvv@gmail.com>
2022-04-29 13:08:53 +0300

    refactoring ver 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,83 +1,85 @@
 package com.company;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        StringBuilder sb = new StringBuilder();
-
+        //Создание папок
         //src
-        //Создадим объекты File для файлов и каталогов:
-        File dirSrc = new File("D://00_java//All_My_Project//Games//src");
-        // пробуем создать каталог
-        if (dirSrc.mkdir())    sb.append("Каталог src создан ** ");
+        createFolder("D://00_java//All_My_Project//Games//src");
 
         //res
-        File dirRes = new File("D://00_java//All_My_Project//Games//res");
-        if (dirRes.mkdir())    sb.append("Каталог res создан ** ");
+        createFolder("D://00_java//All_My_Project//Games//res");
 
         //savegames
-        File dirSavegames = new File("D://00_java//All_My_Project//Games//savegames");
-        if (dirSavegames.mkdir())    sb.append("Каталог savegames создан ** ");
+        createFolder("D://00_java//All_My_Project//Games//savegames");
 
         //temp
-        File dirTemp = new File("D://00_java//All_My_Project//Games//temp");
-        if (dirTemp.mkdir())    sb.append("Каталог temp создан ** ");
+        createFolder("D://00_java//All_My_Project//Games//temp");
 
         //main
-        File dirMain = new File("D://00_java//All_My_Project//Games//src//main");
-        if (dirMain.mkdir())    sb.append("Каталог main создан ** ");
+        createFolder("D://00_java//All_My_Project//Games//src//main");
 
         //test
-        File dirTest = new File("D://00_java//All_My_Project//Games//src//test");
-        if (dirTest.mkdir())    sb.append("Каталог test создан ** ");
-
-        //Main.java
-        // определяем объект для файла
-        File myFileMain = new File("D://00_java//All_My_Project//Games//src//main//Main.java");
-        // создадим новый файл
-        try {if (myFileMain.createNewFile())
-            sb.append("Файл Main.java создан ** ");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        //Utils.java
-        File myFileUtils = new File("D://00_java//All_My_Project//Games//src//main//Utils.java");
-        try {if (myFileUtils.createNewFile())
-            sb.append("Файл Utils.java создан ** ");
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+        createFolder("D://00_java//All_My_Project//Games//src//test");
 
         //drawables
-        File dirDrawables = new File("D://00_java//All_My_Project//Games//res//drawables");
-        if (dirDrawables.mkdir())    sb.append("Каталог drawables создан ** ");
+        createFolder("D://00_java//All_My_Project//Games//res//drawables");
 
         //vectors
-        File dirVectors = new File("D://00_java//All_My_Project//Games//res//vectors");
-        if (dirVectors.mkdir())    sb.append("Каталог vectors создан ** ");
+        createFolder("D://00_java//All_My_Project//Games//res//vectors");
 
         //icons
-        File dirIcons = new File("D://00_java//All_My_Project//Games//res//icons");
-        if (dirIcons.mkdir())    sb.append("Каталог icons создан ** ");
+        createFolder("D://00_java//All_My_Project//Games//res//icons");
+
+
+        //Создание файлов
+        //Main.java
+        createFile("D://00_java//All_My_Project//Games//src//main//Main.java");
+
+        //Utils.java
+        createFile("D://00_java//All_My_Project//Games//src//main//Utils.java");
 
         //temp.txt
-        File myFileTemp = new File("D://00_java//All_My_Project//Games//temp//Temp.txt");
-        try {if (myFileTemp.createNewFile())
-            sb.append("Файл temp.txt создан ** ");
+        createFile("D://00_java//All_My_Project//Games//temp//Temp.txt");
+    }
+
+    public static void createFolder(String pathName) {
+        File myDir = new File(pathName);
+        if (myDir.mkdir()) {
+            System.out.println("Каталог " + pathName + " создан");
+            writeLogProgramm("Каталог " + pathName + " создан");
+        } else {
+            System.out.println("ОШИБКА СОЗДАНИЯ КАТАЛОГА " + pathName);
+            writeLogProgramm("ОШИБКА СОЗДАНИЯ КАТАЛОГА " + pathName);
+        }
+    }
+
+    public static void createFile(String fileName) {
+        File myFileName = new File(fileName);
+        // создадим новый файл
+        try {
+            if (myFileName.createNewFile() || myFileName.exists()) {
+                System.out.println("Файл " + fileName + " создан");
+                writeLogProgramm("Файл " + fileName + " создан");
+            } else {
+                System.out.println("ОШИБКА СОЗДАНИЯ ФАЙЛА " + fileName);
+                writeLogProgramm("ОШИБКА СОЗДАНИЯ ФАЙЛА " + fileName);
+            }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
+    }
 
-        System.out.println(sb);
-
-        String logText = sb.toString();
-        try (FileWriter writer = new FileWriter(myFileTemp, false)) {
+    public static void writeLogProgramm(String myTextLog) {
+        File myLogFile = new File("D://00_java//All_My_Project//Games//temp//Temp.txt");
+        try (FileWriter writer = new FileWriter(myLogFile, true)) {
             // запись всей строки
-            writer.write(logText);
+            writer.write(myTextLog);
             // запись по символам
             writer.append('\n');
             writer.append('!');
