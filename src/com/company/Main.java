04c9c326ee4a5eeffbb43bde510ff2d3d3d76953
ChinenOFF <pro100cvv@gmail.com>
2022-05-13 12:53:28 +0300

    refactoring ver 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,49 +3,38 @@ package com.company;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
         //Создание папок
-        //src
-        createFolder("D://00_java//All_My_Project//Games//src");
-
-        //res
-        createFolder("D://00_java//All_My_Project//Games//res");
-
-        //savegames
-        createFolder("D://00_java//All_My_Project//Games//savegames");
-
-        //temp
-        createFolder("D://00_java//All_My_Project//Games//temp");
-
-        //main
-        createFolder("D://00_java//All_My_Project//Games//src//main");
-
-        //test
-        createFolder("D://00_java//All_My_Project//Games//src//test");
-
-        //drawables
-        createFolder("D://00_java//All_My_Project//Games//res//drawables");
-
-        //vectors
-        createFolder("D://00_java//All_My_Project//Games//res//vectors");
-
-        //icons
-        createFolder("D://00_java//All_My_Project//Games//res//icons");
-
+        ArrayList<String> arrayFolders = new ArrayList<>();
+        arrayFolders.add("D://00_java//All_My_Project//Games//src");
+        arrayFolders.add("D://00_java//All_My_Project//Games//res");
+        arrayFolders.add("D://00_java//All_My_Project//Games//savegames");
+        arrayFolders.add("D://00_java//All_My_Project//Games//temp");
+        arrayFolders.add("D://00_java//All_My_Project//Games//src//main");
+        arrayFolders.add("D://00_java//All_My_Project//Games//src//test");
+        arrayFolders.add("D://00_java//All_My_Project//Games//res//drawables");
+        arrayFolders.add("D://00_java//All_My_Project//Games//res//vectors");
+        arrayFolders.add("D://00_java//All_My_Project//Games//res//icons");
+        arrayFolders.add("D://00_java//All_My_Project//Games//res//icons");
+        for (String folder : arrayFolders
+             ) {
+            createFolder(folder);
+        }
 
         //Создание файлов
-        //Main.java
-        createFile("D://00_java//All_My_Project//Games//src//main//Main.java");
-
-        //Utils.java
-        createFile("D://00_java//All_My_Project//Games//src//main//Utils.java");
-
-        //temp.txt
-        createFile("D://00_java//All_My_Project//Games//temp//Temp.txt");
+        ArrayList<String> arrayFile = new ArrayList<>();
+        arrayFile.add("D://00_java//All_My_Project//Games//src//main//Main.java");
+        arrayFile.add("D://00_java//All_My_Project//Games//src//main//Utils.java");
+        arrayFile.add("D://00_java//All_My_Project//Games//temp//Temp.txt");
+        for (String file : arrayFile
+             ) {
+            createFile(file);
+        }
     }
 
     public static void createFolder(String pathName) {
